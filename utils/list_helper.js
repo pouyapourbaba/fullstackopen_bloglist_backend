0b54e95cffe0a249f26c78abc25183515278a325
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-06-28 12:49:09 +0300

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = blogs => {
   return blogs.map(blog => blog.likes).reduce((a, b) => a + b);
 };
 
+const favoriteBlog = blogs => {
+  const sortedByLikes = blogs.sort((a, b) => a.likes < b.likes);
+  const favorite = {
+    title: sortedByLikes[0].title,
+    author: sortedByLikes[0].author,
+    likes: sortedByLikes[0].likes
+  };
+  return favorite;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
